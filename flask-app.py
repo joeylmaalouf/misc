3a0c9f3adfd0ba5b0629b9dfacf8ac6ca76b0ce0
joeylmaalouf <joeylmaalouf@gmail.com>
2015-05-27 18:56:18 -0400

    :|

--- flask-app.py
+++ flask-app.py
@@ -19,9 +19,13 @@ def show_user_profile(username):
 
 @app.route("/post/<int:post_id>/")
 def show_post(post_id):
-	return "Post #{0}".format(post_id)
+	if post_id in posts:
+		return posts[post_id]
+	else:
+		return "Error: could not find post #{0}.".format(post_id)
 
 
 if __name__ == "__main__":
+	posts = {0: "hello", 1: "world", 2: ":D", 3: ":|"}
 	app.run(host = "0.0.0.0", debug = True)
 	# http://flask.pocoo.org/docs/0.10/quickstart/
