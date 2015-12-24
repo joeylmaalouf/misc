ec33fc30f561f13ba1ac6390e3a10e38183822da
joeylmaalouf <joeylmaalouf@gmail.com>
2015-12-23 19:38:20 -0500

    redid selenium stuff

--- browser.py
+++ browser.py
@@ -1,23 +1,37 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from sys import argv
 from time import sleep
 
 
-def main(argv):
-	if len(argv) < 2:
-		argv.append("selenium python")
+def lmgtfy(query):
+  driver = webdriver.Firefox()
+  driver.maximize_window()
+  driver.get("https://www.google.com")
+  assert "Google" in driver.title, "Cannot access Google."
+  search_box = driver.find_element_by_name("q")
+  for key in query:
+    search_box.send_keys(key)
+    sleep(0.1)
+  search_box.send_keys(Keys.RETURN)
+  sleep(2)
+  driver.close()
 
-	browser = webdriver.Firefox()
-	browser.get("http://www.google.com")
-	assert "Google" in browser.title
 
-	elem = browser.find_element_by_name("q")  # Find the search box
-	elem.send_keys(" ".join(argv[1:]) + Keys.RETURN)
-
-	sleep(5)
-	browser.quit()
+def open_pages(urls):
+  driver = webdriver.Firefox()
+  driver.maximize_window()
+  for i, url in enumerate(urls):
+    driver.get(url)
+    # assert url == driver.current_url, "Redirected!"
+    if i < len(urls) - 1:
+      driver.find_element_by_tag_name("body").send_keys(Keys.CONTROL + "t") 
+  # driver.quit()
 
 
 if __name__ == "__main__":
-	main(argv)
+  lmgtfy("never gonna give you up")
+  open_pages([
+    "https://www.reddit.com/r/SmashBros",
+    "https://www.reddit.com/r/SSBPM",
+    "https://www.reddit.com/r/DotA2"
+  ])
